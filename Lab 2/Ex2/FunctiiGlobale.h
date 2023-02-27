42977cea5345c5f75f8d91a3bd393f6c1137744e
mariacioratanu <119046697+mariacioratanu@users.noreply.github.com>
2023-02-27 22:23:56 +0200

    Add files via upload

---
+++ Lab 2/Ex2/FunctiiGlobale.h
@@ -0,0 +1,7 @@
+#include "StudentProperties.h"
+
+int comparaNume(const Student& student1, const Student &student2);
+int comparaNotaEngleza(const Student& student1, const Student& student2);
+int comparaNotaIstorie(const Student& student1, const Student& student2);
+int comparaNotaMate(const Student& student1, const Student &student2);
+int comparaNotaFinala(const Student& student1, const Student& student2);
