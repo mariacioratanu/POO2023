fd80d7e477072006c48ae039f6091830dc4196db
mariacioratanu <119046697+mariacioratanu@users.noreply.github.com>
2023-02-28 09:12:21 +0200

    Update FunctiiGlobale.cpp

--- Lab 2/Ex2/FunctiiGlobale.cpp
+++ Lab 2/Ex2/FunctiiGlobale.cpp
@@ -2,7 +2,11 @@
 #include "StudentProperties.h"
 #include <cstring>
 
-int comparaNume(const Student& student1, const Student& student2) {
+//compara cei doi studenti(numele, notele la fiecare materie si media finala)
+//daca studentii sunt egali se returneaza 0, daca primul este mai mare se returneaza 1, iar in caz contrar, -1
+
+int comparaNume(const Student& student1, const Student& student2) //compar numele studentilor folosind functia strcmp
+{
     int rezultat = strcmp(student1.getNume(), student2.getNume());
     if (rezultat==0)
         return 0;
@@ -11,7 +15,8 @@ int comparaNume(const Student& student1, const Student& student2) {
     return 1;
 }
 
-int comparaNotaMate(const Student& student1, const Student& student2) {
+int comparaNotaMate(const Student& student1, const Student& student2)
+{
     if (student1.getNotaMate() == student2.getNotaMate())
         return 0;
     else if (student1.getNotaMate() > student2.getNotaMate())
@@ -20,7 +25,8 @@ int comparaNotaMate(const Student& student1, const Student& student2) {
         return -1;
 }
 
-int comparaNotaEngleza(const Student& student1, const Student& student2) {
+int comparaNotaEngleza(const Student& student1, const Student& student2) 
+{
     if (student1.getNotaEngleza() == student2.getNotaEngleza())
         return 0;
     else if (student1.getNotaEngleza() > student2.getNotaEngleza())
@@ -29,7 +35,8 @@ int comparaNotaEngleza(const Student& student1, const Student& student2) {
         return -1;
 }
 
-int comparaNotaIstorie(const Student& student1, const Student& student2) {
+int comparaNotaIstorie(const Student& student1, const Student& student2) 
+{
     if (student1.getNotaIstorie() == student2.getNotaIstorie())
         return 0;
     else if (student1.getNotaIstorie() > student2.getNotaIstorie())
@@ -38,7 +45,8 @@ int comparaNotaIstorie(const Student& student1, const Student& student2) {
         return -1;
 }
 
-int compareNotaFinala(const Student& student1, const Student& student2) {
+int compareNotaFinala(const Student& student1, const Student& student2) 
+{
     if (student1.getNotaFinala() == student2.getNotaFinala())
         return 0;
     else if (student1.getNotaFinala() > student2.getNotaFinala())
