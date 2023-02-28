182c40e611c7ed5fd2f33e63cd864e29087c2792
mariacioratanu <119046697+mariacioratanu@users.noreply.github.com>
2023-02-28 09:13:08 +0200

    Update FunctiiGlobale.h

--- Lab 2/Ex2/FunctiiGlobale.h
+++ Lab 2/Ex2/FunctiiGlobale.h
@@ -1,5 +1,7 @@
 #include "StudentProperties.h"
 
+//5 functii globale utilizate pentru compararea studentilor
+
 int comparaNume(const Student& student1, const Student &student2);
 int comparaNotaEngleza(const Student& student1, const Student& student2);
 int comparaNotaIstorie(const Student& student1, const Student& student2);
