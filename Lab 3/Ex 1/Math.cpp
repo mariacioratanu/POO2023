a81a212dd15fe256e27a0e541757b5f9c95617b4
Maria Ciorâțanu <119046697+mariacioratanu@users.noreply.github.com>
2023-03-05 14:55:59 +0200

    Add files via upload

---
+++ Lab 3/Ex 1/Math.cpp
@@ -0,0 +1,79 @@
+#include "Math.h"
+#include <iostream>
+#include <cstring>
+//codul sursa pentru clasa Math
+int Math::Add(int a, int b)
+{
+    return a+b;
+}
+
+int Math::Add(int a, int b, int c)
+{
+    return a+b+c;
+}
+
+int Math::Add(double a, double b)
+{
+    return a+b;
+}
+
+int Math::Add(double a, double b, double c)
+{
+    return a+b+c;
+}
+
+int Math::Mul(int a, int b)
+{
+    return a*b;
+}
+
+int Math::Mul(int a, int b, int c)
+{
+    return a*b*c;
+}
+
+int Math::Mul(double a, double b)
+{
+    return a*b;
+}
+
+int Math::Mul(double a, double b, double c)
+{
+    return a*b*c;
+}
+
+int Math::Add(int count, ...)
+{
+    int suma=0, i, *p=&count;
+    for(i=0;i<count;i++)
+    {
+        p=p+2;
+        suma=suma+*p;
+    }
+    return suma;
+}
+
+char* Math::Add(const char* a, const char* b)
+{
+    int ok=1, n1, n2;
+    if(a==nullptr)
+    {
+        a="";
+        ok=0;
+    }
+    if(b==nullptr)
+    {
+        b="";
+        ok=0;
+    }
+    n1=strlen(a);
+    n2=strlen(b);
+    char* c=new char[n1+n2+1]; //aloca memorie care poate aduna cele doua siruri existente
+    strcpy(c, "");
+    strcat(c, a);
+    strcat(c, b); //concateneaza sirurile a si b
+
+    c[n1+n2+1]='\0';
+    if(ok==0) return nullptr; //daca unul dintre siruri este nullptr, se va returna nullptr
+    else return c;
+}
