f4433aab73966aa0712c7b97cc33d91bb0041c1c
Maria Ciorâțanu <119046697+mariacioratanu@users.noreply.github.com>
2023-03-10 23:01:17 +0200

    Update Canvas.cpp

--- Lab 3/Ex 2/Canvas.cpp
+++ Lab 3/Ex 2/Canvas.cpp
@@ -21,65 +21,71 @@ Canvas::Canvas(int width, int height)
 
 void Canvas::DrawCircle(int x, int y, int raza, char caracter)
 {
-    int R2=raza*raza;
-    for (int xvar=0; xvar < this->height; xvar++)
-        for (int yvar=0; yvar < this->width; yvar++)
+    int xvar, yvar;
+    int r2=raza*raza;
+    for(xvar=0; xvar<this->height; xvar++)
+        for(yvar=0; yvar<this->width; yvar++)
         {
-            int D = pow(xvar - x, 2) + pow(yvar - y, 2);
-            if (D >= R2 - raza/2 && D <= R2 + raza/2)
+            int D=pow(xvar-x, 2)+pow(yvar-y, 2);
+            if(D>=r2-raza/2 && D<=r2+raza/2)
                 this->SetPoint(xvar, yvar, caracter);
         }
 }
 
-void Canvas::FillCircle(int x, int y, int ray, char ch)
+void Canvas::FillCircle(int x, int y, int raza, char caracter)
 {
-    int R2 = ray * ray;
-    for (int xvar = 0; xvar < this->height; xvar++)
-        for (int yvar = 0; yvar < this->width; yvar++)
+    int xvar, yvar;
+    int r2=raza*raza;
+    for(xvar=0; xvar<this->height; xvar++)
+        for(yvar=0; yvar<this->width; yvar++)
         {
-            int D = pow(xvar - x, 2) + pow(yvar - y, 2);
-            if (D <= R2 + ray/2)
-                this->SetPoint(xvar, yvar, ch);
+            int D=pow(xvar-x, 2)+pow(yvar-y, 2);
+            if(D<=r2+raza/2)
+                this->SetPoint(xvar, yvar, caracter);
         }
 }
 
-void Canvas::DrawRect(int left, int top, int right, int bottom, char ch)
+void Canvas::DrawRect(int left, int top, int right, int bottom, char caracter)
 {
-    for (int x = top; x<=bottom; x++)
-        for (int y = left; y<=right;y++)
+    int x, y;
+    for(x=top; x<=bottom; x++)
+        for(y=left; y<=right;y++)
         {
-            if (x == top || x == bottom || y == left || y == right)
-                this->SetPoint(x, y, ch);
+            if(x==top || x==bottom || y==left || y==right)
+                this->SetPoint(x, y, caracter);
         }
 }
 
-void Canvas::FillRect(int left, int top, int right, int bottom, char ch)
+void Canvas::FillRect(int left, int top, int right, int bottom, char caracter)
 {
-    for (int x = top; x <= bottom; x++)
-        for (int y = left; y <= right; y++)
+    int x, y;
+    for(x=top; x<=bottom; x++)
+        for(y=left; y<=right; y++)
         {
-                this->SetPoint(x, y, ch);
+                this->SetPoint(x, y, caracter);
         }
 }
-void Canvas::SetPoint(int x, int y, char ch)
+void Canvas::SetPoint(int x, int y, char caracter)
 {
-    this->canvas[x][y]=ch;
+    this->canvas[x][y]=caracter;
 }
 
 void Canvas::DrawLine(int x1, int y1, int x2, int y2, char ch)
 {
-    int dx = x2 - x1;
-    int dy = y2 - y1;
-    int D  = 2 * dy - dx;
-    int y  = y1;
-
-    for (int x=x1;x<=x2;x++) {
+    int dx=x2-x1;
+    int dy=y2-y1;
+    int D=2*dy-dx;
+    int y=y1;
+    int x;
+    for(x=x1;x<=x2;x++)
+    {
         this->SetPoint(x, y, ch);
-        if (D > 0) {
+        if(D>0)
+        {
             y++;
-            D = D - 2 * dx;
+            D=D-2*dx;
         }
-        D = D + 2 * dy;
+        D=D+2*dy;
     }
 }
 
