f3520c711fc4ef1988af2e4d02024176857b2cfc
Maria Ciorâțanu <119046697+mariacioratanu@users.noreply.github.com>
2023-03-20 17:29:26 +0200

    Update main.cpp

--- Lab 3/Ex 1/main.cpp
+++ Lab 3/Ex 1/main.cpp
@@ -5,18 +5,18 @@ using namespace std;
 int main()
 {
     Math op=Math();
-    cout<<op.Add(5, 6)<<endl;
-    cout<<op.Add(3, 4, 5)<<endl;
-    cout<<op.Add(7.5, 8.5)<<endl;
-    cout<<op.Add(6.5, 7.5, 8.5)<<endl;
-    cout<<op.Mul(1, 2)<<endl;
-    cout<<op.Mul(1, 2, 3)<<endl;
-    cout<<op.Mul(4.5, 5.5)<<endl;
-    cout<<op.Mul(7.5, 8.5, 9.5)<<endl;
-    cout<<op.Add(4, 1, 2, 3, 4)<<endl;
-    cout<<op.Add("buna", "ziua")<<endl;
-    cout<<op.Add(nullptr, nullptr)<<endl;
-    cout<<op.Add(nullptr, "ziua")<<endl;
-    cout<<op.Add("buna", nullptr)<<endl;
+    std::cout<<op.Add(5, 6)<<endl;
+    std::cout<<op.Add(3, 4, 5)<<endl;
+    std::cout<<op.Add(7.5, 8.5)<<endl;
+    std::cout<<op.Add(6.5, 7.5, 8.5)<<endl;
+    std::cout<<op.Mul(1, 2)<<endl;
+    std::cout<<op.Mul(1, 2, 3)<<endl;
+    std::cout<<op.Mul(4.5, 5.5)<<endl;
+    std::cout<<op.Mul(7.5, 8.5, 9.5)<<endl;
+    std::cout<<op.Add(4, 1, 2, 3, 4)<<endl;
+    std::cout<<op.Add("buna", "ziua")<<endl;
+    std::cout<<op.Add(nullptr, nullptr)<<endl;
+    std::cout<<op.Add(nullptr, "ziua")<<endl;
+    std::cout<<op.Add("buna", nullptr)<<endl;
     return 0;
 }
