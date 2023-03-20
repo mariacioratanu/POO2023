fe9efd8a4a0461d5cab35317b16a525130d45b4f
Maria Ciorâțanu <119046697+mariacioratanu@users.noreply.github.com>
2023-03-20 17:33:28 +0200

    Update main.cpp

--- Lab 4/main.cpp
+++ Lab 4/main.cpp
@@ -13,8 +13,8 @@ int main()
     Sort type2(v, 6);
     type2.BubbleSort(true); //sorted: 1, 2, 3, 4, 6, 8
     type2.Print();
-    cout<<type2.GetElementsCount()<<endl; //number of elements: 6
-    cout<<type2.GetElementFromIndex(2)<<endl; //element from index 2: 3
+    std::cout<<type2.GetElementsCount()<<endl; //number of elements: 6
+    std::cout<<type2.GetElementFromIndex(2)<<endl; //element from index 2: 3
 
     Sort type3({5,20,10,30,60,50,40,70}); 
     type3.QuickSort(); //sorted: 70, 60, 50, 40, 30, 20, 10, 5
@@ -23,6 +23,6 @@ int main()
     Sort type4(8, -25, 25); //size=8, min=-25, max=25
     type4.QuickSort(true); //sorted: -18, -17, -12, -12, -11, -5, -3, 20
     type4.Print();
-    cout<<type4.GetElementsCount()<<endl; //number of elements: 8
-    cout<<type4.GetElementFromIndex(4)<<endl; //element from index 4:-11
+    std::cout<<type4.GetElementsCount()<<endl; //number of elements: 8
+    std::cout<<type4.GetElementFromIndex(4)<<endl; //element from index 4:-11
 }
