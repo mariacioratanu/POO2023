bb0b569a6c01849eccca3465481f2ef4db6d5f40
Maria Ciorâțanu <119046697+mariacioratanu@users.noreply.github.com>
2023-03-26 22:36:35 +0300

    Update main.cpp

--- Lab 7/Ex 1/main.cpp
+++ Lab 7/Ex 1/main.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
 
-float operator""_Kelvin(unsigned long long int kelvin)
+//two literal operators, _Kelvin and _Fahrenheit, which allow
+//to write numeric literals suffixed with either _Kelvin or _Fahrenheit
+//to represent temperatures in either Kelvin or Fahrenheit
+
+float operator""_Kelvin(unsigned long long int kelvin) //the numeric value of the temperature in Kelvin
 {
-    return static_cast<float>(kelvin-273.15);
+    return static_cast<float>(kelvin-273.15); //returns a float value(the temperature in Celsius)
 }
 
-float operator""_Fahrenheit(unsigned long long int fahrenheit)
+float operator""_Fahrenheit(unsigned long long int fahrenheit) //the numeric value of the temperature in Fahrenheit
 {
-    return static_cast<float>((fahrenheit-32)/1.8);
+    return static_cast<float>((fahrenheit-32)/1.8); //returns a float value(the temperature in Celsius)
 }
 
 int main()
 {
     float a=300_Kelvin;
     float b=120_Fahrenheit;
-    std::cout<<"300 degrees Kelvin is "<<a<<" degrees Celsius"<< std::endl;
-    std::cout<<"120 degrees Fahrenheit is "<<b<<" degrees Celsius"<< std::endl;
+    std::cout<<"300 degrees Kelvin is "<<a<<" degrees Celsius"<< std::endl; //26.85 C
+    std::cout<<"120 degrees Fahrenheit is "<<b<<" degrees Celsius"<< std::endl; //48.8889 C
     return 0;
 }
