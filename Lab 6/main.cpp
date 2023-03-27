c8f34c4d891eea0263344ea886e25773f6504b2d
Maria Ciorâțanu <119046697+mariacioratanu@users.noreply.github.com>
2023-03-27 13:02:10 +0300

    Update main.cpp

--- Lab 6/main.cpp
+++ Lab 6/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include "Weather.h"
 #include "Circuit.h"
-#include "Dacia.h"
-#include "Toyota.h"
+#include "Volvo.h"
+#include "Seat.h"
 #include "BMW.h"
-#include "Skoda.h"
-#include "Ford.h"
+#include "Fiat.h"
+#include "RangeRover.h"
 #include "Car.h"
 
 int main()
@@ -13,11 +13,11 @@ int main()
     Circuit c;
     c.SetLength(100); //a circuit with a length of 100 km
     c.SetWeather(Weather::Sunny); //race weather conditions: rain
-    c.AddCar(new Dacia());
-    c.AddCar(new Toyota());
+    c.AddCar(new Volvo());
     c.AddCar(new BMW());
-    c.AddCar(new Ford());
-    c.AddCar(new Skoda());
+    c.AddCar(new Seat());
+    c.AddCar(new Fiat());
+    c.AddCar(new RangeRover());
     c.Race();
     c.ShowFinalRanks();//it will print the time each car needed to finish the circuit sorted from the fastest car to the slowest
     c.ShowWhoDidNotFinish();//it is possible that some cars don't have enough fuel to finish the circuit
