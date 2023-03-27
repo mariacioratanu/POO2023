4854abdf0f804e9316425cbf28a478526c7fb0d4
Maria Ciorâțanu <119046697+mariacioratanu@users.noreply.github.com>
2023-03-27 13:09:27 +0300

    Update and rename Skoda.cpp to Fiat.cpp

--- Lab 6/Fiat.cpp
+++ Lab 6/Fiat.cpp
@@ -1,28 +1,28 @@
-#include "Skoda.h"
+#include "Fiat.h"
 
-Skoda::Skoda() : Car(50, 10)
+Fiat::Fiat() : Car(50, 10)
 {
     this->average_speed[(int) Weather::Rain]=100;
     this->average_speed[(int) Weather::Snow]=30;
     this->average_speed[(int) Weather::Sunny]=155;
 }
 
-const char* Skoda::getName()
+const char* Fiat::getName()
 {
-    return "Skoda";
+    return "Fiat";
 }
 
-double Skoda::getFuelCapacity()
+double Fiat::getFuelCapacity()
 {
     return this->fuel_capacity;
 }
 
-double Skoda::getFuelConsumption()
+double Fiat::getFuelConsumption()
 {
     return this->fuel_consumption;
 }
 
-double Skoda::getAverageSpeed(Weather weather)
+double Fiat::getAverageSpeed(Weather weather)
 {
     return this->average_speed[(int)weather];
 }
